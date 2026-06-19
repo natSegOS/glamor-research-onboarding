417d3fba8f15947059b709f69523337795e9b5c9
Nathan Segura <nathan.es@icloud.com>
2026-06-19 11:30:20 -0700

    Added __init__ for inference submodule

---
+++ experiments/001_typo_robustness/src/inference/__init__.py
@@ -0,0 +1,23 @@
+"""The inference sub-package: the model roster and the generation engines.
+
+Re-exports the full public surface of both modules:
+
+    from inference import MODEL_ROSTER, get_model_specification
+    from inference import build_inference_engine, VllmEngine
+"""
+
+from inference.roster import (
+    REVISION_PLACEHOLDER,
+    ModelSpecification,
+    MODEL_ROSTER,
+    get_model_specification,
+    resolve_current_revision,
+    assert_revisions_pinned,
+)
+
+from inference.engines import (
+    VllmEngine,
+    HuggingFaceEngine,
+    build_inference_engine,
+)
+
